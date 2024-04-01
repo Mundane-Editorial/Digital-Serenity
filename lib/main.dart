44c6df9344fc09950fab3cb5d276b481c8599b9d
Mundane Editorial <uday2k17@rediffmail.com>
2024-04-01 09:27:35 +0530

    added display icon

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Digital Serenity'),
+        // title: const Text('Digital Serenity'),
       ),
       body: Center(
         child: Column(
