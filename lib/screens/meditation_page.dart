6d71edfc6db736e03a099ee723d7d2c6a7741b34
Mundane Editorial <uday2k17@rediffmail.com>
2024-04-07 19:35:35 +0530

    Meditation page completed

--- lib/screens/meditation_page.dart
+++ lib/screens/meditation_page.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:add_image_in_app/screens/my_profile.dart';
 import 'package:add_image_in_app/screens/onboarding_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class MeditationPage extends StatefulWidget {
   const MeditationPage({super.key});
@@ -10,6 +14,55 @@ class MeditationPage extends StatefulWidget {
 }
 
 class _MeditationPageState extends State<MeditationPage> {
+  final player1 = AudioPlayer();
+  final player2 = AudioPlayer();
+  final player3 = AudioPlayer();
+  bool _isPlaying1 = false;
+  bool _isPlaying2 = false;
+  bool _isPlaying3 = false;
+  bool _isRepeatEnabled = false;
+  // double _sliderValue = 0;
+  // double slideValue = 0.0;
+
+  void _togglePlayPause1() {
+    setState(() {
+      _isPlaying1 = !_isPlaying1;
+      if (_isPlaying1) {
+        playSound1();
+      } else {
+        pauseSound1();
+      }
+    });
+  }
+
+  void _togglePlayPause2() {
+    setState(() {
+      _isPlaying2 = !_isPlaying2;
+      if (_isPlaying2) {
+        playSound2();
+      } else {
+        pauseSound2();
+      }
+    });
+  }
+
+  void _togglePlayPause3() {
+    setState(() {
+      _isPlaying3 = !_isPlaying3;
+      if (_isPlaying3) {
+        playSound3();
+      } else {
+        pauseSound3();
+      }
+    });
+  }
+
+  void _toggleRepeat() {
+    setState(() {
+      _isRepeatEnabled = !_isRepeatEnabled;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,7 +99,7 @@ class _MeditationPageState extends State<MeditationPage> {
                             ),
                           ),
                           SizedBox(height: 1),
-                          Text("Want to reduce your screen time"),
+                          Text("Meditate and relax!"),
                         ],
                       ),
                       const Spacer(),
@@ -143,15 +196,15 @@ class _MeditationPageState extends State<MeditationPage> {
                                     shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(25.0),
                                     ),
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      _togglePlayPause1();
+                                    },
                                     backgroundColor: Colors.white,
                                     label: const Text("Play"),
-                                    icon: const Icon(
-                                      Icons.play_arrow,
-                                      color: Colors.lightBlue,
+                                    icon: _isPlaying1 ? Icon(Icons.pause, color: Colors.blue ) : Icon(Icons.play_arrow,  color: Colors.blue)
                                     ),
                                   ),
-                                ),
+                                // );
                               ],
                             ),
                           ),
@@ -207,8 +260,11 @@ class _MeditationPageState extends State<MeditationPage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          "Stress Relaxing",
-                          style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.w500),
+                          "Stress Release",
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500),
                         ),
                         Text(
                           "10 Minutes",
@@ -217,49 +273,49 @@ class _MeditationPageState extends State<MeditationPage> {
                       ],
                     ),
                     const Spacer(),
-                    FloatingActionButton(
-                      onPressed: () {},
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25.0),
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.lightBlueAccent,
                       ),
-                      backgroundColor: Colors.lightBlueAccent,
-                      mini: true, // Make the button mini
-                      child: const Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.play_arrow,
-                            color: Colors.white,
-                          ),
-                        ],
+                      child: IconButton(
+                        onPressed: () {
+                          _togglePlayPause2();
+                          // Add your logic for playing or pausing audio here
+                          // player.play('songs/stress.mp3' as Source);
+                        },
+                        icon: _isPlaying2
+                            ? Icon(Icons.pause, color: Colors.white)
+                            : Icon(Icons.play_arrow, color: Colors.white),
                       ),
                     ),
-
 
                   ],
                 ),
                 const SizedBox(height: 10),
-              const Divider(
-                color: Colors.black38,
-                indent: 10,
-                endIndent: 10,
-              ),
-                const SizedBox(height: 4),
-
-              const Padding(
-                padding: EdgeInsets.only(left: 8.0),
-                child: Text(
-                  "Activities",
-                  style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.black87,
-                      fontWeight: FontWeight.w800),
+                const Divider(
+                  color: Colors.black38,
+                  indent: 10,
+                  endIndent: 10,
                 ),
-              ),
-              const SizedBox(height: 10),
+                const SizedBox(height: 4),
+                const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    "Activities",
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w800),
+                  ),
+                ),
+                const SizedBox(height: 10),
                 Container(
                   height: 200,
-                  padding: const EdgeInsets.only(bottom: 2.0),
+                  padding: const EdgeInsets.only(
+                    bottom: 2.0,
+                    top: 5,
+                  ),
                   child: ListView(
                     scrollDirection: Axis.horizontal,
                     children: [
@@ -288,13 +344,18 @@ class _MeditationPageState extends State<MeditationPage> {
                                     color: Colors.white,
                                     fontWeight: FontWeight.w800),
                               ),
-                              const SizedBox(height: 8,),
-                              Expanded(child: Image.asset("assets/images/focus.png"))
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Expanded(
+                                  child: Image.asset("assets/images/focus.png"))
                             ],
                           ),
                         ),
                       ),
-                      const SizedBox(width: 7,),
+                      const SizedBox(
+                        width: 7,
+                      ),
                       Container(
                         width: 140,
                         decoration: BoxDecoration(
@@ -320,13 +381,18 @@ class _MeditationPageState extends State<MeditationPage> {
                                     color: Colors.white,
                                     fontWeight: FontWeight.w800),
                               ),
-                              const SizedBox(height: 40,),
-                              Expanded(child: Image.asset("assets/images/relax.png"))
+                              const SizedBox(
+                                height: 40,
+                              ),
+                              Expanded(
+                                  child: Image.asset("assets/images/relax.png"))
                             ],
                           ),
                         ),
                       ),
-                      const SizedBox(width: 7,),
+                      const SizedBox(
+                        width: 7,
+                      ),
                       Container(
                         width: 140,
                         decoration: BoxDecoration(
@@ -352,8 +418,12 @@ class _MeditationPageState extends State<MeditationPage> {
                                     color: Colors.white,
                                     fontWeight: FontWeight.w800),
                               ),
-                              const SizedBox(height: 8,),
-                              Expanded(child: Image.asset("assets/images/healing.png"))
+                              const SizedBox(
+                                height: 28,
+                              ),
+                              Expanded(
+                                  child:
+                                      Image.asset("assets/images/healing.png"))
                             ],
                           ),
                         ),
@@ -361,13 +431,17 @@ class _MeditationPageState extends State<MeditationPage> {
                     ],
                   ),
                 ),
-                const SizedBox(height: 8,),
+                const SizedBox(
+                  height: 8,
+                ),
                 const Divider(
                   color: Colors.black38,
                   indent: 10,
                   endIndent: 10,
                 ),
-                const SizedBox(height: 4,),
+                const SizedBox(
+                  height: 4,
+                ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
@@ -385,18 +459,140 @@ class _MeditationPageState extends State<MeditationPage> {
                         ),
                       ],
                     ),
-
+                    // child: IconButton(
+                    //   onPressed: ()  {
+                    //      playSound();
+                    //   },
+                    //   child: Text("Play"),
+                    // ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 17.0),
+                          child: Container(
+                            width: 90,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.8),
+                                  spreadRadius: 2,
+                                  blurRadius: 7,
+                                  offset: const Offset(0, 0),
+                                ),
+                              ],
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.asset(
+                                'assets/images/thumbnail.png',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                child: const Text(
+                                  'Canon D',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                // Align text to the left
+                                child: const Text(
+                                  'Pachelbel Canon In D Major',
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ),
+                              //TODO: Slider
+                              // slider
+                              Align(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.skip_previous),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {
+                                        _togglePlayPause3();
+                                        // Add your logic for playing or pausing audio here
+                                        // player.play(
+                                        //     'songs/Canon_D.mp3' as Source);
+                                      },
+                                      icon: _isPlaying3
+                                          ? Icon(Icons.pause)
+                                          : Icon(Icons.play_arrow),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.skip_next),
+                                    ),
+                                    IconButton(
+                                      onPressed: _toggleRepeat,
+                                      icon: _isRepeatEnabled
+                                          ? Icon(Icons.repeat)
+                                          : Icon(Icons.repeat_one),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 )
-
-
               ],
             ),
           ),
-
         ],
       ),
-    
     );
+  }
+
+
+  Future<void> playSound1() async {
+    var soundPath1 = "songs/meditation.mp3";
+    await player1.play(AssetSource(soundPath1));
+  }
+
+  Future<void> playSound2() async {
+    var soundPath2 = "songs/stress.mp3";
+    await player2.play(AssetSource(soundPath2));
+  }
+
+  Future<void> playSound3() async {
+    var soundPath = "songs/Canon_D.mp3";
+    await player3.play(AssetSource(soundPath));
+  }
+
+  Future<void> pauseSound1() async {
+    await player1.pause();
+  }
+
+  Future<void> pauseSound2() async {
+    await player2.pause();
+  }
+
+  Future<void> pauseSound3() async {
+    await player3.pause();
   }
 }
